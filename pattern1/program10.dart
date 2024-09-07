9cb2fc473b64420ba350d5f9664dbdf47ef9932d
Krutika <154690388+Krutika-03@users.noreply.github.com>
2024-09-08 00:16:23 +0530

    Add files via upload

---
+++ pattern1/program10.dart
@@ -0,0 +1,19 @@
+import "dart:io";
+void main(){
+  print("Enter the number of rows:");
+  int? rows=int.parse(stdin.readLineSync()!);
+
+  int number=1;
+
+  for(int i=0; i<rows; i++){
+   number =i+1;
+    for(int j=0; j<rows; j++){
+      stdout.write(" $number ");
+      if(j!=rows-1){
+      number++;
+    }
+    }
+    print("  ");
+    
+  }
+}
